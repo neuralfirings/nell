826ac565c052c4f04d43335562f84e62767925f8
Nancy Yi Liang <hi@nyl.io>
2024-06-05 12:24:43 -0700

    bugfix

--- app/routes/admin.adc.tsx
+++ app/routes/admin.adc.tsx
@@ -6,31 +6,25 @@ import fs from 'fs'
 
 export const action: ActionFunction = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData()
-  const filepath = formData.get('filepath') as string
+  const key = formData.get('key') as string
+  if (key != process.env.NELL_ADMIN_KEY) {
+    return json({status: "error", error: "invalid key"})
+  }
+  const filepath = process.env.NODE_ENV == "production" ? "/app/lovehart-bea11be05900.json" : "./local/fake-lovehart-bea11be05900.json"
   const content = formData.get('content') ? formData.get('content') as string : ""
 
-  if (filepath != null && filepath != "")
+  try {
     fs.writeFileSync(filepath, content, 'utf8')
+    return json({status: "success"})
+  }
+  catch (err) {
+    return json({status: "error", error: err})
+  }
 
-  // do stuff
-  return null
 }
 
 export const loader: LoaderFunction = async  ({ request, params }: LoaderFunctionArgs) => {
-  const url = new URL(request.url)
-  const urlParams = Object.fromEntries(url.searchParams);
-  const key = urlParams.key
-  const dir = urlParams.dir
-  const file = urlParams.file
-
-  if (key != process.env.NELL_ADMIN_KEY) {
-    return {}
-  }
-  
-  const listDir = dir ? fs.readdirSync(dir) : "none"
-  const listFile = file ? fs.readFileSync(dir + "/" + file, 'utf8') : "none"
-  
-  return json({ listDir, listFile})
+  return json({})
 }
 
 export default function Page() {
@@ -38,22 +32,18 @@ export default function Page() {
   const loaderData = useLoaderData<typeof loader>();
   const navigation = useNavigation();
 
-
   return (
     <Container>
-      <Title order={2}>Dir List</Title>  
-      <Code block>
-        {JSON.stringify(loaderData.listDir, null, 2)}
-      </Code>
-      <Title mt="sm" order={2}>File</Title>  
-      <Code block>
-        {loaderData.listFile}
-      </Code>
-      <Title mt="sm" order={2}>Write</Title>  
+      {actionData?.status === 'success' && (
+        <Alert color="green" mb="md">Success</Alert>
+      )}
+      {actionData?.status === 'error' && (
+        <Alert color="red" mb="md">Error: {actionData.error}</Alert>
+      )}
       <Form method="post">
-        <TextInput required name="filepath" label="Filepath" />
+        <TextInput name="key" label="Key" type="password" />
         <Textarea name="content" label="Content" rows={10} />
-        <Button variant="primary" mt="md" type="submit">Write to File</Button>
+        <Button variant="primary" mt="md" type="submit">Submit</Button>
       </Form>
     </Container>
   )
